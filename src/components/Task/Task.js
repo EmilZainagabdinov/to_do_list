b729cc90ef2f3a4834ea49d0320794ed4292171f
Emil Zainagabdinov <screw192@gmail.com>
2020-12-17 23:37:07 +0600

    Added task status related className and checkbox character.

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Task = props => {
+  let taskClass;
+  let checkBox;
+
+  if (props.taskStatus) {
+    taskClass = 'task';
+    checkBox = '\u2610';
+  } else if (!props.taskStatus) {
+    taskClass = 'task taskFinished';
+    checkBox = '\u2611';
+  }
+
   return (
-      <div className="task">
-        <p className='taskText'>{props.taskText}</p>
+      <div className='taskBody'>
+        <div className={taskClass}>
+          <span className='checkBox' onClick={props.done}>{checkBox}</span>
+          <p>{props.taskText}</p>
+        </div>
         <button className='removeButton' onClick={props.remove}>&#10005;</button>
       </div>
   )
